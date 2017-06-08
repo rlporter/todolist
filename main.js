f8418f4277aab9f4aab12bcd32a3223933813b89
Rebekkah Porter <rebekkah1@me.com>
2017-06-08 16:00:32 -0400

    submit individual activity

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // Add your JS here.
+inputElement.addEventListener("keypress", newTodo, true); {
+    if (keyCode == 13);
+}
+  function newTodo(e) {
+  let element = document.querySelector('.todo-List');
+  let listItem = document.createElement('li');
+  listItem.textContent = " ";
+  console.log(listItem);
+  element.appendChild( listItem );
+  var div = document.createElement("div");
+  document.body.appendChild(div);
+  element.classList.add("view");
+
+  }
+  return newTodo;
+
+}
